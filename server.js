504bcbaf9039aac2b0dd6495f105297af182cf90
Johannes Zellner <johannes@nebulon.de>
2022-07-12 11:57:09 +0200

    Add place selector

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 'use strict';
 
+const path =  require('path'),
+    fs = require('fs');
+
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
@@ -13,33 +16,49 @@ server.listen(3000);
 var users = [];
 var strokes = [];
 
+const STORAGE_PATH = path.join(__dirname, 'paintings');
+
+function ensureStoragePath() {
+    fs.mkdirSync(STORAGE_PATH, { recursive: true });
+}
+
+ensureStoragePath();
+
 io.sockets.on('connection', function (socket) {
-  console.log('new Connection');
+    console.log('new Connection');
 
-	socket.emit('userlist', users);
-	socket.emit('init', strokes);
+    socket.emit('userlist', users);
+    socket.emit('init', strokes);
 
-	const name = 'user_'+Math.floor(Math.random()*101);
-	users.push(name);
-	socket.emit('user', {userid: name});
-	io.sockets.emit('user connected', {userid: name});
+    const name = 'user_'+Math.floor(Math.random()*101);
+    users.push(name);
+    socket.emit('user', {userid: name});
+    io.sockets.emit('user connected', {userid: name});
 
-	socket.on('draw', function(data){
-		strokes.push(data);
-		socket.broadcast.emit('draw', data);
-	});	
-	
-	socket.on('mousedown', function(data){
-		socket.broadcast.emit('mousedown', data);
-	});	
-	
-	socket.on('color', function(data){
-		socket.broadcast.emit('color', data);
-	});
+    socket.on('draw', function (data) {
+        strokes.push(data);
+        socket.broadcast.emit('draw', data);
+    });
 
-	socket.on('clear', function () {
-		console.log('clearing canvas');
-		strokes = [];
-		socket.broadcast.emit('clear');
-	});
+    socket.on('mousedown', function (data) {
+        socket.broadcast.emit('mousedown', data);
+    });
+
+    socket.on('color', function (data) {
+        socket.broadcast.emit('color', data);
+    });
+
+    socket.on('clear', function () {
+        console.log('clearing canvas');
+        strokes = [];
+        socket.broadcast.emit('clear');
+    });
+
+    socket.on('save', function () {
+        ensureStoragePath();
+
+        const canvasPath = path.join(STORAGE_PATH, Date.now() + '.csv');
+        console.log('saving canvas to', canvasPath);
+        fs.writeFileSync(canvasPath, strokes.filter(function (s) { return s.x && s.y && s.color; }).map(function (s) { return `${s.x},${s.y},${s.color}`; }).join('\n') + '\n');
+    });
 });
